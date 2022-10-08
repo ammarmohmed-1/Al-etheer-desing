c0cabe4b479c77036bb65d35f28e1b74b54b3c97
ammarmohmed-1 <ammarmlmero@gmail.com>
2022-10-08 15:44:54 +0200

    thound commit

--- script/script.js
+++ script/script.js
@@ -5,3 +5,8 @@ var nav_none = document.querySelector(".nav-none");
 icon_bars.addEventListener("click",()=>{
     nav_none.classList.toggle("nav-block")
 })
+
+window.onscroll = ()=>{
+    nav_none.classList.remove("nav-block")
+
+}
